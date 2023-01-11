a5ae9114ef638bfefe6e49d3bc455ab0fc3a8ceb
Theo-C <theo.capitaine2@gmail.com>
2023-01-11 22:04:49 +0100

    premier test de ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Je viens de faire une modif !
         </p>
         <a
           className="App-link"
